09b2a5fdba20f86d5296faa0db3eee91220bbfdb
Mayela <mancheta@cpp.edu>
2022-10-20 14:17:40 -0700

    First version test

---
+++ RestaurantBill.c
@@ -0,0 +1,15 @@
+
+//Restaurant Bill
+
+#include <stdio.h>
+
+int main()
+{
+
+printf("Hi! Welcome in folks!\n What can I get you started with?");
+printf("\nMenu\n");
+
+printf("Salad: $9.95");
+    
+    return 0;
+}
