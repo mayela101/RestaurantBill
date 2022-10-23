6e13ea19a5da9fd0eff3a94a6c6ab39c18b34f02
Mayela <mancheta@cpp.edu>
2022-10-23 15:31:23 -0700

    Add

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -6,10 +6,19 @@
 int main()
 {
 
-printf("Hi! Welcome in folks!\n What can I get you started with?");
-printf("\nMenu\n");
+printf("Hi! Welcome in folks!\n");
+printf("What can I get you started with?");
+printf("\n");
+printf("\nMENU\n");
 
-printf("Salad: $9.95");
+printf("1. Salad: $9.95\n");
+printf("2. Soup: $4.55\n");
+printf("3. Sandwich: $13.25\n");
+printf("4. Pizza: $22.35\n");
+
+
+
+
     
     return 0;
 }
