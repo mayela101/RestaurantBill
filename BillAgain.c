103d41eaebd123ace46984fb4b0d1190456fb1d5
Mayela <mancheta@cpp.edu>
2022-10-23 16:02:56 -0700

    Readding file

---
+++ BillAgain.c
@@ -0,0 +1,25 @@
+
+
+//Restaurant Bill
+
+#include <stdio.h>
+
+int main()
+{
+
+printf("Hi! Welcome in folks!\n");
+printf("What can I get you started with?");
+printf("\n");
+printf("\nMENU\n");
+
+printf("1. Salad: $9.95\n");
+printf("2. Soup: $4.55\n");
+printf("3. Sandwich: $13.25\n");
+printf("4. Pizza: $22.35\n");
+
+
+
+
+    
+    return 0;
+}
