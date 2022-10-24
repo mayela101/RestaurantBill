22f8cb38f97017e61d716041f9dd488b4756ea61
mayela101 <113002207+mayela101@users.noreply.github.com>
2022-10-23 17:08:36 -0700

    Update RestaurantBill.c

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -6,17 +6,31 @@
 int main()
 {
 
-printf("Hi! Welcome in folks!\n");
-printf("What can I get you started with?");
-printf("\n");
-printf("\nMENU\n");
+/*
+Begin with simple introduction + list menu items 
+
+whole idea is that it's an actual person taking your order and whatnot
+
+"what can I get you started with"
+
 
 printf("1. Salad: $9.95\n");
 printf("2. Soup: $4.55\n");
 printf("3. Sandwich: $13.25\n");
 printf("4. Pizza: $22.35\n");
 
-
+*/
+    
+//customer will then select an item through number (1-4)
+// so a total of 3 scanf statements 
+ /*   > meal choice
+      > tax
+      > tip (as a waitress this should be like 50% but alright)
+      
+ */
+    
+ /* since im reading in a choice by # on the menu use switch statements to show the outputs for each item*/
+    
 
 
     
